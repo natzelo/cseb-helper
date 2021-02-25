8ab0a5267be9e71d27547181103512dd96d1450f
ryuzaki221 <52002984+ryuzaki221@users.noreply.github.com>
2021-02-25 15:32:28 +0530

    Added keys.js

---
+++ keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://dbUser:Ryuzakiisl@cluster0-shard-00-00.xz1ok.mongodb.net:27017,cluster0-shard-00-01.xz1ok.mongodb.net:27017,cluster0-shard-00-02.xz1ok.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-12rkgv-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 require("./db/mongoose");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 const PORT = process.env.PORT || 5000;
 
@@ -14,6 +16,10 @@ const householdRouter = require("./routers/household");
 
 app.use("/api", userRouter);
 app.use("/api", householdRouter);
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`"Listening on port ${PORT}`);
